401ae82728899919ea03f1d5bd31d4e6fc2cc3d5
Vishal Kotekar <108614877+VishalKotekar@users.noreply.github.com>
2023-03-28 02:48:17 +0530

    Update settings.py

--- school_app/settings.py
+++ school_app/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = "school_app.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    # }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
 }
 
 
